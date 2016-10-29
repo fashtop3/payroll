ed7c2622043e9fef3f125dccd3e61e8edf76eaa7
Fashola Ayodeji <fashtop3@gmail.com>
2016-10-29 09:14:05 +0100

    Added laravel mongoDB package

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -29,18 +29,6 @@
         }]);
 })();
 /**
- * Created by dfash on 10/10/16.
- */
-(function () {
-    angular
-        .module('monarchApp')
-        .controller('OrderController', ['$scope', '$rootScope', function($scope, $rootScope) {
-
-            $rootScope.closedSidebar = true;
-            $rootScope.hideTopNav = true;
-        }]);
-})();
-/**
  * Created by dfash on 9/22/16.
  */
 
@@ -158,6 +146,18 @@
                 }
             }
 
+        }]);
+})();
+/**
+ * Created by dfash on 10/10/16.
+ */
+(function () {
+    angular
+        .module('monarchApp')
+        .controller('OrderController', ['$scope', '$rootScope', function($scope, $rootScope) {
+
+            $rootScope.closedSidebar = true;
+            $rootScope.hideTopNav = true;
         }]);
 })();
 //# sourceMappingURL=controllers.js.map
