ddb9aa84953e08d6173adb7973a1877bb9b0b3ac
Fashola Ayodeji <fashtop3@gmail.com>
2016-11-21 13:58:31 +0100

    modified overtime formular [Profile.php]

--- app/Employee/Profile.php
+++ app/Employee/Profile.php
@@ -133,7 +133,7 @@ class Profile extends Model
 
         //Todo: make the constant 23 to be variable
         if ($paytype->label == 'OVERTIME') {
-            return (double) ($basic_pay/23) / 8;
+            return (double) (($basic_pay/23) / 8) * $paytype->value;
         }
 
         return (double) $rate = ($basic_pay/23) * $paytype->value;
