21cfb8762ae1c3b784bd6dbd0743b499d35c5b0e
Fashola Ayodeji <fashtop3@gmail.com>
2016-12-16 10:18:47 +0100

    changed [wkhtmltopdf] vendor to linux

--- config/snappy.php
+++ config/snappy.php
@@ -5,17 +5,16 @@ return array(
 
     'pdf' => array(
         'enabled' => true,
-        'binary'  => '/usr/local/bin/wkhtmltopdf',
-       // 'binary'  => base_path('vendor/h4cc/wkhtmltopdf-win32/bin/wkhtmltopdf.exe'),
-        //'binary'  => base_path('C:/Program Files/wkhtmltopdf/bin/wkhtmltopdf'),
+        //'binary'  => '/usr/local/bin/wkhtmltopdf',
+        'binary'  => base_path('vendor/h4cc/wkhtmltopdf-amd64/bin/wkhtmltopdf-amd64'),
         'timeout' => false,
         'options' => array(),
         'env'     => array(),
     ),
     'image' => array(
         'enabled' => true,
-        'binary'  => '/usr/local/bin/wkhtmltoimage',
-//        'binary'  => base_path('C:/Program Files/wkhtmltoimage/bin/wkhtmltoimage.exe'),
+//        'binary'  => '/usr/local/bin/wkhtmltoimage',
+        'binary'  => base_path('vendor/h4cc/wkhtmltoimage-amd64/bin/wkhtmltoimage-amd64'),
         'timeout' => false,
         'options' => array(),
         'env'     => array(),
